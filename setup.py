916aa71ccee96f8dd19933acaf82b7f6c2fab418
ryanpepper <ryan.pepper1@gmail.com>
2017-02-13 19:00:41 +0000

    Add optimisation flag

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension("test", ["test.pyx"], )]
+ext_modules = [Extension("test", ["test.pyx"], extra_compile_args=['-O3'])]
 
 setup(
   name = 'Hello world app',
